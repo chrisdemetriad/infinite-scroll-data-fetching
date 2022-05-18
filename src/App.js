40299b09bf1a022f0c5eec8e508fe12b878f73a8
Chris Demetriad <chris@demetriad.co.uk>
2022-05-18 10:20:51 +0100

    Implementing infinity scrolling functionality

--- src/App.js
+++ src/App.js
@@ -1,51 +1,89 @@
-import "./App.css";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
+
+const TOTAL_PAGES = 3;
+const BEERS_PER_PAGE = 25;
 
 const App = () => {
-	const [data, setData] = useState(null);
-	const [page] = useState(1);
+	const [beers, setBeers] = useState([]);
+	const [page, setPage] = useState(1);
+	const [loading, setLoading] = useState(true);
+	const [last, setLast] = useState(null);
 
-	const BEERS_PER_PAGE = 80;
 	const API_URL = `https://api.punkapi.com/v2/beers?page=${page}&per_page=${BEERS_PER_PAGE}`;
+
+	const observer = useRef(
+		new IntersectionObserver((beers) => {
+			const first = beers[0];
+			if (first.isIntersecting) {
+				setPage((number) => number + 1);
+			}
+		})
+	);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const result = await axios.get(API_URL);
-				const data = await result.data;
-				setData(data);
+				setLoading(true);
+				let response = await axios.get(API_URL);
+				let allBeers = [...beers, ...response.data];
+				setBeers([...allBeers]);
+				setLoading(false);
 			} catch (err) {
 				// @todo: handle error
 			}
 		};
 
-		fetchData();
-	}, []);
+		if (TOTAL_PAGES >= page) {
+			fetchData();
+		}
+	}, [page]);
+
+	useEffect(() => {
+		const current = last;
+		const currentObserver = observer.current;
+
+		if (current) {
+			currentObserver.observe(current);
+		}
+
+		return () => {
+			if (current) {
+				currentObserver.unobserve(current);
+			}
+		};
+	}, [last]);
 
 	return (
-		<div>
-			{data ? (
-				data.length > 0 ? (
-					<ul>
-						{data.map((beer) => (
+		<>
+			{loading && <p>Loading</p>}
+			{beers.length > 0 &&
+				beers.map((beer, index) => {
+					return !loading && index === beers.length - 1 && TOTAL_PAGES >= page ? (
+						<div key={beer.id} id={beer.id} ref={setLast}>
 							<li key={beer.id}>
-								{/* <img src={beer.image_url} alt={beer.name} /> */}
+								<img src={beer.image_url} alt={beer.name} />
 								<p>
 									{beer.name} - {beer.tagline}
 								</p>
 								<p>{beer.description}</p>
 								<p>{beer.food_pairing}</p>
 							</li>
-						))}
-					</ul>
-				) : (
-					<h2>Loading</h2>
-				)
-			) : (
-				<h2>No results</h2>
-			)}
-		</div>
+						</div>
+					) : (
+						<li key={beer.id}>
+							<img src={beer.image_url} alt={beer.name} />
+							<p>
+								{beer.name} - {beer.tagline}
+							</p>
+							<p>{beer.description}</p>
+							<p>{beer.food_pairing}</p>
+						</li>
+					);
+				})}
+			{TOTAL_PAGES === page - 1 && <p>No more results to fetch</p>}
+		</>
 	);
 };
+
 export default App;
